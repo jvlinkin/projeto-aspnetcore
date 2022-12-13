e21a9fa31f6d97544a30ab775f13af849f18613b
JoaoPaiva <jvlimapaiva@gmail.com>
2022-12-13 20:23:52 -0300

    Primeiro commit

---
+++ Context/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using LanchesJa.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LanchesJa.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {        
+        }
+
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Lanche> Lanches { get; set; }
+    }
+}
