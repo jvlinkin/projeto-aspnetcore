e979ceec8ec3c9a3ab1c4cf474431aebc5cc34a6
JoaoPaiva <jvlimapaiva@gmail.com>
2022-12-14 14:43:01 -0300

    criando view pagina lanches

---
+++ Controllers/LancheController.cs
@@ -0,0 +1,21 @@
+﻿using LanchesJa.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanchesJa.Controllers
+{
+    public class LancheController : Controller
+    {
+        private readonly ILancheRepository _lancheRepository;
+
+        public LancheController(ILancheRepository lancheRepository)
+        {
+            _lancheRepository = lancheRepository;
+        }
+
+        public IActionResult List()
+        {
+            var lanches = _lancheRepository.Lanches;
+            return View(lanches);
+        }
+    }
+}
