e589b550bc6e276fe3bb2261278177ef2615708b
JoaoPaiva <jvlimapaiva@gmail.com>
2022-12-16 14:54:21 -0300

    feat: criando o componente (carrinho) e renderizando na navbar .

---
+++ Components/CarrinhoCompraResumo.cs
@@ -0,0 +1,37 @@
+﻿using LanchesJa.Models;
+using LanchesJa.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanchesJa.Components
+{
+	public class CarrinhoCompraResumo : ViewComponent
+	{
+		private readonly CarrinhoCompra _carrinhoCompra;
+
+		public CarrinhoCompraResumo(CarrinhoCompra carrinhoCompra)
+		{
+			_carrinhoCompra= carrinhoCompra;
+		}
+		public IViewComponentResult Invoke()
+		{
+			//Recuperando os itens do carrinho
+			var itens = _carrinhoCompra.GetCarrinhoCompraItens();
+
+			//var itens = new List<CarrinhoCompraItem>()
+			//{
+			//	new CarrinhoCompraItem(),
+			//	new CarrinhoCompraItem()
+			//};	
+
+			//Jogando os itens recuperados, para dentro dos itens do carrinho;
+			_carrinhoCompra.CarrinhoCompraItems = itens;
+
+			var carrinhoCompraViewModel = new CarrinhoCompraViewModel
+			{
+				CarrinhoCompra = _carrinhoCompra,
+				CarrinhoCompraTotal = _carrinhoCompra.GetCarrinhoCompraTotal(),
+			};
+			return View(carrinhoCompraViewModel);
+		}
+	}
+}
