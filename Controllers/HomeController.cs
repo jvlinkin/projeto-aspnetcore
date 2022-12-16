b752253fdfb3d68df901d53c5c8851e0090e68a1
JoaoPaiva <jvlimapaiva@gmail.com>
2022-12-16 12:22:18 -0300

    feat: criando a viewModel, e a partial view onde estarão exibindo os lanches preferidos.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,17 +1,31 @@
 ﻿using LanchesJa.Models;
+using LanchesJa.Repositories;
+using LanchesJa.Repositories.Interfaces;
+using LanchesJa.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
 namespace LanchesJa.Controllers
 {
-    public class HomeController : Controller
+	
+	public class HomeController : Controller
     {
-        
+		private readonly ILancheRepository _lancheRepository;
 
-        public IActionResult Index()
+		public HomeController(ILancheRepository lancheRepository)
+		{
+			_lancheRepository = lancheRepository;
+		}
+
+
+		public IActionResult Index()
         {
+            var homeViewModel = new HomeViewModel
+            {
+                LanchesPreferidos = _lancheRepository.LanchesPreferidos
+            };
            
-            return View();
+            return View(homeViewModel);
         }
 
         
---
+++ ViewModels/HomeViewModel.cs
@@ -0,0 +1,9 @@
+﻿using LanchesJa.Models;
+
+namespace LanchesJa.ViewModels
+{
+	public class HomeViewModel
+	{
+		public IEnumerable<Lanche> LanchesPreferidos { get; set; }
+	}
+}
