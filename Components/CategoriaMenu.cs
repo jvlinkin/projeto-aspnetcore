56c501a015931a09bc9104a7b5023c6d45fcc12f
JoaoPaiva <jvlimapaiva@gmail.com>
2022-12-19 14:33:53 -0300

    feat: criação do dropdown do menu de categorias

---
+++ Components/CategoriaMenu.cs
@@ -0,0 +1,23 @@
+﻿using LanchesJa.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanchesJa.Components
+{
+	public class CategoriaMenu : ViewComponent
+	{
+		private readonly ICategoriaRepository _categoriaRepository;
+
+		public CategoriaMenu(ICategoriaRepository categoriaRepository)
+		{
+			_categoriaRepository = categoriaRepository;
+		}
+
+		public IViewComponentResult Invoke() 
+		{
+			var categorias = _categoriaRepository.Categorias.OrderBy(c => c.CategoriaNome);
+			return View(categorias);
+		}
+
+
+	}
+}
