6ef297f4fa35a691681168a55e8e7837a3ba168a
JoaoPaiva <jvlimapaiva@gmail.com>
2022-12-20 17:27:03 -0300

    feat: criando a tela de login, e validando os campos

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,12 +1,54 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using LanchesJa.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace LanchesJa.Controllers
 {
 	public class AccountController : Controller
 	{
-		public IActionResult Index()
+		private readonly UserManager<IdentityUser> _userManager;
+		private readonly SignInManager<IdentityUser> _signInManager;
+
+		public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
 		{
-			return View();
+			_userManager = userManager;
+			_signInManager = signInManager;
+		}
+
+		public IActionResult Login(string returnUrl)
+		{
+			return View(new LoginViewModel()
+			{
+				ReturnUrl = returnUrl,
+
+			});
+		}
+		[HttpPost]
+		public async Task<IActionResult> Login(LoginViewModel loginVM)
+		{
+			if (!ModelState.IsValid)
+			{
+				return View(loginVM);
+			};
+
+			var user = await _userManager.FindByNameAsync(loginVM.UserName);
+
+			if (user != null)
+			{
+				//Aqui ele valida se o usuário que foi encontrado, contém a mesma senha que o usuário está passando na viewModel.
+				var result = await _signInManager.PasswordSignInAsync(user, loginVM.Password, false, false);
+
+				if (result.Succeeded)
+				{
+					if (string.IsNullOrEmpty(loginVM.ReturnUrl))
+					{
+						return RedirectToAction("Index", "Home");
+					}
+					return Redirect(loginVM.ReturnUrl);
+				}
+			}
+			ModelState.AddModelError("", "Falha ao realizar o login!");
+			return View(loginVM);
 		}
 	}
 }
