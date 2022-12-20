4422b2d0331a7e0e5ce25ece8990202fcc6a2114
JoaoPaiva <jvlimapaiva@gmail.com>
2022-12-20 11:45:16 -0300

    feat: criando formulario de checkout (conclusão de pedido)

---
+++ Controllers/PedidoController.cs
@@ -0,0 +1,31 @@
+﻿using LanchesJa.Models;
+using LanchesJa.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanchesJa.Controllers
+{
+    public class PedidoController : Controller
+    {
+        private readonly IPedidoRepositorio _pedidoRepositorio;
+        private readonly CarrinhoCompra _carrinhoCompra;
+
+        public PedidoController(IPedidoRepositorio pedidoRepositorio,
+                                 CarrinhoCompra carrinhoCompra)
+        {
+            _pedidoRepositorio = pedidoRepositorio;
+            _carrinhoCompra = carrinhoCompra;
+        }
+        [HttpGet]
+        public IActionResult Checkout()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Checkout(Pedido pedido)
+        {
+            return View();
+        }
+
+    }
+}
