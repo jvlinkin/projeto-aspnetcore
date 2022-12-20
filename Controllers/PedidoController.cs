a5998a53bfcbe307679e6281619fbc2ec526be21
JoaoPaiva <jvlimapaiva@gmail.com>
2022-12-20 13:30:59 -0300

    feat: lógica para criar pedido

--- Controllers/PedidoController.cs
+++ Controllers/PedidoController.cs
@@ -30,6 +30,41 @@ namespace LanchesJa.Controllers
             //Obter os itens do carrinho de compra do cliente
             List<CarrinhoCompraItem> itens = _carrinhoCompra.GetCarrinhoCompraItens();
             _carrinhoCompra.CarrinhoCompraItems = itens;
+
+            //Verificando se existem itens no carrinho
+            if(_carrinhoCompra.CarrinhoCompraItems.Count == 0)
+            {
+                ModelState.AddModelError("", "Seu carrinho está vazio. Que tal incluir um lanche?");
+            }
+
+            //calcula o total de itens e o total do pedido
+            foreach (var item in itens)
+            {
+                totalItensPedido += item.Quantidade;
+                precoTotalPedido += (item.Lanche.Preco * item.Quantidade);
+            }
+
+            //Atribuir os valores obtidos do pedido
+            pedido.TotalItensPedido = totalItensPedido;
+            pedido.PedidoTotal = precoTotalPedido;
+
+            //Validar os dados do pedido
+            if (ModelState.IsValid)
+            { 
+                //criar o pedido e os detalhes
+                _pedidoRepositorio.CriarPedido(pedido);
+
+                //define mensagens ao cliente
+                ViewBag.CheckouCompletoMensagem = "Obrigado pelo seu pedido :)";
+                ViewBag.TotalPedido = _carrinhoCompra.GetCarrinhoCompraTotal();
+
+                //Limpar o carrinho do cliente
+                _carrinhoCompra.LimparCarrinho();
+
+                //Exibe a view com dados do cliente e do pedido
+                return View("~/Views/Pedido/CheckoutCompleto.cshtml", pedido);
+            }
+            return View(pedido);
         }
 
     }
