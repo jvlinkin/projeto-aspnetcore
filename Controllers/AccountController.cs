babb6dbd2ca254e8c5966ced789ef6ecd9cebf86
JoaoPaiva <jvlimapaiva@gmail.com>
2022-12-20 17:45:08 -0300

    feat: criando o método POST para registrar um usuário.

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -50,5 +50,36 @@ namespace LanchesJa.Controllers
 			ModelState.AddModelError("", "Falha ao realizar o login!");
 			return View(loginVM);
 		}
+
+		
+		public IActionResult Register()
+		{
+			return View();
+
+		}
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Register(LoginViewModel registerVM)
+		{
+			if (ModelState.IsValid)
+			{
+				var user = new IdentityUser { UserName = registerVM.UserName };
+
+				var result = await _userManager.CreateAsync(user, registerVM.Password);
+
+				if (result.Succeeded)
+				{
+					//await _signInManager.SignInAsync(user, isPersistent: false);
+					return RedirectToAction("Login", "Account");
+				}
+				else
+				{
+					ModelState.AddModelError("Registro", "Falha ao registrar o usuário");
+				}
+			}
+			return View(registerVM);
+
+		}
 	}
 }
